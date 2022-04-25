8d5cbac69f7ed3424ad00b0a70aee074264fb9ef
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-04-26 05:10:55 +0800

    :beers: <pre> and &nbsp; for preserving spaces :beers:

---
+++ Chapter1/demo_DOCTYPE.sh
@@ -0,0 +1,10 @@
+The DOCTYPE tells the browser what rules to follow when showing the document to the user.
+
+These rules are called the modes.
+
+HTML5 DOCTYPE used in this book, that puts the browser into strict mode.
+
+Another mode is called quirks mode.
+
+
+HTML5 doctype looks like <!DOCTYPE html>
